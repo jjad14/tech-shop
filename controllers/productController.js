2cea03dcdb5a432ddfc7a00b7b7e3fa3f8be22d3
Dennis <dmarlo96@live.ca>
2021-05-08 22:37:57 -0400

    Product Controller

---
+++ controllers/productController.js
@@ -0,0 +1,34 @@
+import asyncHandler from 'express-async-handler';
+
+import Product from '../models/Product.js';
+
+// GET api/products
+// Get all products
+// Public access
+
+const getProducts = asyncHandler(async (req, res) => {
+    const products = await Product.find({});
+
+    // @todo: return error if no products
+
+    res.json(products);
+});
+
+// GET api/products/:id
+// Get product by id
+// Public access
+const getProductById = asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id);
+
+    if (!product) {
+      return res.status(404).json({ message: 'No Product Found' });
+    }
+
+    res.json(product);
+
+});
+
+export {
+    getProducts,
+    getProductById
+};
--- routes/products.js
+++ routes/products.js
@@ -1,43 +1,10 @@
 import express from 'express';
-import asyncHandler from 'express-async-handler';
-
-import Product from '../models/Product.js';
+import { getProducts, getProductById} from '../controllers/productController.js';
 
 const router = express.Router();
 
-// GET api/products
-// Get all products
-// Public access
-router.get(
-  '/',
-  asyncHandler(async (req, res) => {
-    const products = await Product.find({});
-
-    // @todo: return error if no products
-
-    res.json(products);
-  })
-);
-
-// GET api/products/:id
-// Get product by id
-// Public access
-router.get(
-  '/:id',
-  asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id);
-
-    if (!product) {
-      return res.status(404).json({ message: 'No Product Found' });
-    }
-
-    res.json(product);
-  })
-);
-
-
-
-
+router.route('/').get(getProducts);
+router.route('/:id').get(getProductById);
 
 
 export default router;

9da8b2649b6c35f0e9b1417e515c0ff651ef57cd
Dennis <dmarlo96@live.ca>
2021-05-17 16:40:07 -0400

    Updated Add Order function

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -8,57 +8,47 @@ import Product from '../models/Product.js';
 // Create Order
 // Private access
 const addOrder = asyncHandler(async (req, res) => {
-    const {
+    const { orderItems, shippingAddress, paymentMethod } = req.body;
+
+    if (orderItems.length === 0) {
+      res.status(400);
+      throw new Error("You don't have any products in your cart");
+    }
+
+    // create order instance
+    const order = await Order.create({
       orderItems,
       shippingAddress,
       paymentMethod,
-    } = req.body;
-
-    let beforeTaxes = 0.0;
-    let totalPrice = 0.0;
-    let taxPrice = 0.0;
-    let shippingPrice = 0.0;
-
-    // map each item in cart to determine cost
-    // use backend prices to prevent token manipulation
-    await Promise.all(orderItems.map(async (item) => {
-        // get item by cartitem id
-        let lookupItem = await Product.findById(item.product);
-
-        // overwrite cartItem price with the one from db
-        // prevents cart price tampering
-        item.price = lookupItem.price;
-
-        // calculate taxes
-        taxPrice += (item.price * 0.13) * item.qty;
-
-        // calculate total before tax
-        beforeTaxes += item.price * item.qty;  
-
-        // determine shipping
-        shippingPrice = beforeTaxes < 100 ? 19.99 : 0;
-
-        // determine total cost
-        totalPrice =  beforeTaxes + taxPrice + shippingPrice;
-    }));
-
-    // create order
-    const order = new Order({
-        user: req.user._id,
-        orderItems,
-        shippingAddress,
-        paymentMethod,
-        taxPrice,
-        shippingPrice,
-        totalPrice
+      user: req.user._id,
     });
 
-    // save order
-    const createdOrder = await order.save();
+    // overwrite cartitem image, price and name with db fields
+    for (let item of order.orderItems) {
+      const product = await Product.findById(item.id);
 
-    // return order
-    res.status(201).json(createdOrder);
-    
+      item.image = product.image;
+      item.price = product.price;
+      item.name = product.name;
+    }
+
+    // 
+    order.itemsPrice = Number(
+      order.orderItems
+        .reduce((acc, cur) => acc + cur.price * cur.qty, 0)
+        .toFixed(2)
+    );
+
+    // if order price is less than $100 add $19.99 shipping
+    order.shippingPrice = order.itemsPrice > 100 ? 0 : 19.99;
+    // 13% tax (in ontario)
+    order.taxPrice = Number((order.itemsPrice * 0.13).toFixed(2));
+    // price after taxes and shipping
+    order.totalPrice = order.itemsPrice + order.shippingPrice + order.taxPrice;
+
+    await order.save();
+
+    res.json(order._id);
 });
 
 
